f9b71212f0433e4d713926fe6f852c2e372484a2
Michal Taraszka <michaltaraszka@gmail.com>
2017-08-18 15:58:09 +0200

    Fixed path to public directory

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const settings = require('./settings');
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(settings.PUBLIC_PATH));
 
 app.set('view engine', 'ejs');
 
